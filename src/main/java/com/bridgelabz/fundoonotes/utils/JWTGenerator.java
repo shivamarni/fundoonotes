8c6e9a7c9b0efb802bcacb761c88f6d33bfe6de8
shivamarni <shivamarni10@gmail.com>
2020-02-29 15:57:05 +0530

    [shiva] add. Utilities classes

---
+++ src/main/java/com/bridgelabz/fundoonotes/utils/JWTGenerator.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.fundoonotes.utils;
+
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+@Component
+public class JWTGenerator {
+	public static final String SECRET="1234567890";
+	public  String jwtToken(int l)
+
+	{
+		String token=null;
+		try
+		{
+			token=JWT.create().withClaim("id", l).sign(Algorithm.HMAC512(SECRET));
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return token;
+	}
+	public Integer parseJWT(String jwt)
+	{
+		Integer userId=(Integer)0;
+		if(jwt!=null)
+		{
+			userId=JWT.require(Algorithm.HMAC512(SECRET)).build().verify(jwt).getClaim("id").asInt();
+		}
+		return userId;
+	}
+
+
+}
