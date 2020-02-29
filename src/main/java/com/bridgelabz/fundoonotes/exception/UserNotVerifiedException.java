d8ea6da4404065aab61f7ed9d7570633472db6de
shivamarni <shivamarni10@gmail.com>
2020-02-29 15:51:02 +0530

    [shiva] add. Exceptions

---
+++ src/main/java/com/bridgelabz/fundoonotes/exception/UserNotVerifiedException.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.fundoonotes.exception;
+
+public class UserNotVerifiedException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserNotVerifiedException(String message)
+	{
+		super(message);
+	}
+
+}
