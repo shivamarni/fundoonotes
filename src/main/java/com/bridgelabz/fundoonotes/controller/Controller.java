691dab174611b4cf75a4ffab82fe4805827f31f1
shivamarni <shivamarni10@gmail.com>
2020-03-02 16:03:19 +0530

    [shiva] refractor. updated

--- src/main/java/com/bridgelabz/fundoonotes/controller/Controller.java
+++ src/main/java/com/bridgelabz/fundoonotes/controller/Controller.java
@@ -15,11 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.fundoonotes.dto.UserUpdate;
-import com.bridgelabz.fundoonotes.dto.UserLogin;
 import com.bridgelabz.fundoonotes.dto.UserRegister;
 import com.bridgelabz.fundoonotes.model.UserDemo;
-import com.bridgelabz.fundoonotes.response.ResponseStatus;
-import com.bridgelabz.fundoonotes.response.UserDetails;
+import com.bridgelabz.fundoonotes.response.ResponseMessageStatus;
+import com.bridgelabz.fundoonotes.response.UserInfo;
 import com.bridgelabz.fundoonotes.service.UserService;
 import com.bridgelabz.fundoonotes.utils.JWTGenerator;
 
@@ -29,68 +28,68 @@ public class Controller {
 	@Autowired
 	private UserService userService;
 	@Autowired
-	private JWTGenerator generator;
+	private JWTGenerator jwtGenerator;
 
-	@GetMapping(value="/user/login/{id}")
-	public ResponseEntity<UserDetails> loginUser(@PathVariable String id)
+	@GetMapping(value="/login/{id}")
+	public ResponseEntity<UserInfo> loginUser(@PathVariable String id)
 	{
 		UserDemo result=userService.login(id);
 		if(result!=null)
 		{
-			String token=generator.jwtToken(result.getUserId());
+			String token=jwtGenerator.jwtToken(result.getUserId());
 			return ResponseEntity.status(HttpStatus.ACCEPTED).header("login successful",result.getName())
-					.body(new UserDetails(token,"200 ok",result));
+					.body(new UserInfo(token,"200 ok",result));
 		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new UserDetails("login failed","400 NOT_FOUND",result));
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new UserInfo("login failed","400 NOT_FOUND",result));
 
 	}
-	@PostMapping(value="/user/add-user")
-	public ResponseEntity<ResponseStatus>register(@RequestBody UserRegister userRecord)
+	@PostMapping(value="/register")
+	public ResponseEntity<ResponseMessageStatus>register(@RequestBody UserRegister userRecord)
 	{
 		UserDemo user=userService.register(userRecord);
 		if(user!=null)
 		{
 			return ResponseEntity.status(HttpStatus.CREATED)
-					.body(new ResponseStatus("registration Successful",200,userRecord));
+					.body(new ResponseMessageStatus("registration Successful",200,userRecord));
 		}
 		else
 		{
 
 			return ResponseEntity.status(HttpStatus.ALREADY_REPORTED)
-					.body(new ResponseStatus("Already existing user",400,userRecord));
+					.body(new ResponseMessageStatus("Already existing user",400,userRecord));
 		}
 	}
-	@GetMapping("/user/verify/{token}")
-	public ResponseEntity<ResponseStatus> verify(@PathVariable("token")String token) throws Exception
+	@GetMapping(value="/verify/{token}")
+	public ResponseEntity<ResponseMessageStatus> verify(@PathVariable("token")String token) throws Exception
 	{
-		boolean verification=userService.verify(token);
+		boolean verification=userService.verifyToken(token);
 		if(verification)
 		{
-			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ResponseStatus("verified",200,token));
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ResponseMessageStatus("verified",200,token));
 		}
-		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ResponseStatus("not verified",400,token));
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ResponseMessageStatus("not verified",400,token));
 	}
-	@PutMapping(value="/user/forgot-password")
-	public ResponseEntity<ResponseStatus> forgotPassword(@RequestBody UserUpdate updateDto)
+	@PutMapping(value="/forgot-password")
+	public ResponseEntity<ResponseMessageStatus> forgotPassword(@RequestBody UserUpdate updateDto)
 	{
 		UserDemo result=userService.forgotPassword(updateDto);
 		if(result!=null)
 		{
 			return ResponseEntity.status(HttpStatus.ACCEPTED)
-					.body(new ResponseStatus("password updated successfully",200,User.class));
+					.body(new ResponseMessageStatus("password updated successfully",200,User.class));
 
 		}
 		else
 		{
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseStatus("email not matched",402,updateDto.getEmail()));
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessageStatus("email not matched",402,updateDto.getEmail()));
 		}
 	}
-	@GetMapping("/all-users")
+	@GetMapping("/get-all-users")
 	public List<UserDemo> getAllUsers()
 	{
-		return userService.getUsers();
+		return userService.getAllUsers();
 	}
-	@DeleteMapping(value="/user/delete/{id}")
+	@DeleteMapping(value="/delete/{id}")
 	public void deleteUser(@PathVariable String id)
 	{
 		userService.removeUsers(id);
