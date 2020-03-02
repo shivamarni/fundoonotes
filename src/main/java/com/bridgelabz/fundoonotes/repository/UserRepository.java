384f59c371433f8eef5f6d3d95da3d9d635c8d1c
shivamarni <shivamarni10@gmail.com>
2020-03-02 16:07:30 +0530

    [shiva] refractor. updated

--- src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
+++ src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
@@ -9,23 +9,15 @@ import com.bridgelabz.fundoonotes.model.UserDemo;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserDemo, Integer>{
-	@Query(value="select * from fundoonotes.user where id=?",nativeQuery=true)	
+	@Query(value="select * from user_demo where user_id=?",nativeQuery=true)	
 	public String login();
 
-	@Query(value="insert into fundoonotes.user(id,name,email,mobile,password,isVerified,date) values(?,?,?,?,?,?,?)",nativeQuery=true)
-	public UserDemo register(UserDemo user);
-
-	@Query(value="update fundoonotes.user set password=? where email=?",nativeQuery=true)
-	public UserDemo forgotPassword(UserDemo user);
-
-	@Query(value="select * from fundoonotes.user where id=?",nativeQuery=true)
+	@Query(value="select * from user_demo where user_id=?",nativeQuery=true)
 	public UserDemo getUserById(int id);
 
-	@Query(value="select * from fundoonotes.user where email=?",nativeQuery=true)
+	@Query(value="select * from user_demo where email=?",nativeQuery=true)
 	public UserDemo getUserByEmail(String email);
 
-	@Query(value="update fundoonotes.user set isVerified=true where id=?",nativeQuery=true)
-	public void verify(Integer id);
 
 }
 
