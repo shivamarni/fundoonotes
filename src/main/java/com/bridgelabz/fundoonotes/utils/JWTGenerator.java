9fb9249c84c2ef412e0f322870a5f5bc34d6da38
shivamarni <shivamarni10@gmail.com>
2020-03-12 15:17:45 +0530

    [shiva] refractor. updated

--- src/main/java/com/bridgelabz/fundoonotes/utils/JWTGenerator.java
+++ src/main/java/com/bridgelabz/fundoonotes/utils/JWTGenerator.java
@@ -4,31 +4,30 @@ import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 @Component
 public class JWTGenerator {
-	public static final String SECRET="1234567890";
-	public  String jwtToken(long l)
+	private static final String SECRET = "2129152050365";
 
-	{
-		String token=null;
-		try
-		{
-			token=JWT.create().withClaim("id", l).sign(Algorithm.HMAC512(SECRET));
-		}catch(Exception e)
-		{
+	public String jwtToken(long l) {
+		String token = null;
+		try {
+			token = JWT.create().withClaim("id", l).sign(Algorithm.HMAC512(SECRET));
+		} catch (IllegalArgumentException | JWTCreationException e) {
+
 			e.printStackTrace();
 		}
 		return token;
 	}
-	public Integer parseJWT(String jwt)
-	{
-		Integer userId=(Integer)0;
-		if(jwt!=null)
-		{
-			userId=JWT.require(Algorithm.HMAC512(SECRET)).build().verify(jwt).getClaim("id").asInt();
+
+	public Long parseJWT(String jwt) {
+		Long userId = null;
+		if (jwt != null) {
+			userId = JWT.require(Algorithm.HMAC512(SECRET)).build().verify(jwt).getClaim("id").asLong();
 		}
 		return userId;
 	}
+
 
 
 }
