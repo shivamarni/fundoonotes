bef191303870ddf76f4fd85bb3893c9edc3bc6eb
shivamarni <shivamarni10@gmail.com>
2020-03-12 15:01:47 +0530

    [shiva] refractor. updated

--- src/main/java/com/bridgelabz/fundoonotes/controller/Collabrator.java
+++ src/main/java/com/bridgelabz/fundoonotes/controller/Collabrator.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.fundoonotes.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,12 +9,15 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bridgelabz.fundoonotes.model.NoteInformation;
 import com.bridgelabz.fundoonotes.response.Response;
 import com.bridgelabz.fundoonotes.service.UserCollabrator;
 @RestController
+@RequestMapping("/collabrate")
 public class Collabrator {
 	@Autowired
 	private UserCollabrator service;
@@ -26,7 +31,7 @@ public class Collabrator {
 
 	@GetMapping("/collabrate/getAllCollabrator")
 	public ResponseEntity<Response> getAllCollabrator(@RequestHeader("token") String token) {
-		      service.getAllCollabrators(token);
+		     List<NoteInformation>  noteInfo=service.getAllCollabrators(token);
 		return ResponseEntity.status(HttpStatus.ACCEPTED)
 				.body(new Response("Listed all collabrator information",200));
 	}
