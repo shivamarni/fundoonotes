4ff6d17d8e295477bfba35fb4ee8fcd7e262aa9b
shivamarni <shivamarni10@gmail.com>
2020-03-17 16:23:30 +0530

    [shiva] add. RadishCache class added

---
+++ src/main/java/com/bridgelabz/fundoonotes/configuration/RadishCache.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.fundoonotes.configuration;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import net.sf.ehcache.config.CacheConfiguration;
+@Configuration
+@EnableCaching
+public class RadishCache extends CachingConfigurerSupport {
+	@Bean
+	public net.sf.ehcache.CacheManager ehCacheManager() {
+		
+		CacheConfiguration twentySecondCache = new CacheConfiguration();
+		twentySecondCache.setName("twenty-second-cache");
+		twentySecondCache.setMemoryStoreEvictionPolicy("LRU");
+		twentySecondCache.setMaxEntriesLocalHeap(1000);
+		twentySecondCache.setTimeToLiveSeconds(20);
+
+		net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
+		
+		config.addCache(twentySecondCache);
+		return net.sf.ehcache.CacheManager.newInstance(config);
+	}
+
+	@Bean
+	@Override
+	public CacheManager cacheManager() {
+		
+		return new EhCacheCacheManager(ehCacheManager());
+	}
+}
