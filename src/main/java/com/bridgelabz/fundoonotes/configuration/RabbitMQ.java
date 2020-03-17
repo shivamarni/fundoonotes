37532cc01923ec41feba9ed7e27f4a54de556562
shivamarni <shivamarni10@gmail.com>
2020-03-17 16:22:41 +0530

    [shiva] add. RabbitMQ class added

---
+++ src/main/java/com/bridgelabz/fundoonotes/configuration/RabbitMQ.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.fundoonotes.configuration;
+
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQ {
+	@Value("${fundoo.rabbitmq.queue}")
+	String queueName;
+
+	@Value("${fundoo.rabbitmq.exchange}")
+	String exchange;
+
+	@Value("${fundoo.rabbitmq.routingkey}")
+	private String routingkey;
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	DirectExchange exchange() {
+		return new DirectExchange(exchange);
+	}
+
+	@Bean
+	Binding binding(org.springframework.amqp.core.Queue queue, DirectExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with(routingkey);
+	}
+
+	@Bean
+	public MessageConverter jsonMessageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+}
+	
