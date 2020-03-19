9ff1225ac4f791b6d35bd06062b79dcb4bdb4a75
shivamarni <shivamarni10@gmail.com>
2020-03-19 12:26:40 +0530

    [shiva] refractor. updated

--- src/main/java/com/bridgelabz/fundoonotes/service/AmazonClientService.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.fundoonotes.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface AmazonClientService {
-	void uploadFileToS3Bucket(MultipartFile multipartFile, boolean enablePublicReadAccess,String token);
-
-	void deleteFileFromS3Bucket(String fileName,String token);
-}
