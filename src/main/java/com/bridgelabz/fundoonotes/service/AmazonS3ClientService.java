6f37f61b77f4a4d5cb7545b74c70d458a26cb5e8
shivamarni <shivamarni10@gmail.com>
2020-03-19 12:31:15 +0530

    [shiva] add. AmazonS3ClientService class added

---
+++ src/main/java/com/bridgelabz/fundoonotes/service/AmazonS3ClientService.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.fundoonotes.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AmazonS3ClientService {
+	void uploadFileToS3Bucket(MultipartFile multipartFile, boolean enablePublicReadAccess,String token);
+
+	void deleteFileFromS3Bucket(String fileName,String token);
+}
